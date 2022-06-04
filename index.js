dc949c815079cfcaacf3415756169e154156f075
khalidshah12 <kssshah6@gmail.com>
2022-06-04 16:14:15 +0530

    creat repo

---
+++ index.js
@@ -0,0 +1,82 @@
+
+
+// {/* <iframe width="560" 
+// height="315" 
+// src="https://www.youtube.com/embed/GUdm9n6ikWE" 
+// title="YouTube video player" 
+// frameborder="0" 
+// allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+// allowfullscreen>
+// </iframe> */}
+
+
+
+
+let getData = () => {
+    const searchInput = document.querySelector('#searchInput').value;
+
+    let key = "AIzaSyA1T0Febk63wR1woeiXCEtdQc2tsQSqI2I"
+
+    let url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${searchInput}&key=${key}`
+
+    fetch(url).then(function (res) {
+        return res.json()
+    }).then(function (res) {
+        console.log(res)
+        display(res.items)
+    }).catch(function (err) {
+        console.log(err)
+    })
+}
+
+let display = (data) => {
+    // data.forEach(function ({ id: { videoId }, snippet: { title, description } }) {
+        const movielist = document.querySelector('#preview');
+        movielist.innerHTML = ""
+    data.forEach(function(elem){
+        
+        const movieBox = document.createElement('div');
+
+        const iframe = document.createElement('iframe');
+        iframe.allow = "fullscreen"
+        iframe.src = `https://www.youtube.com/embed/${elem.id.videoId}`
+
+        const title = document.createElement('h4');
+        title.innerText = elem.snippet.title
+
+        // const image = document.createElement('img');
+        // image.src = elem.snippet.thumbnails.medium.url
+
+        movieBox.append(iframe,title)
+        movielist.append(movieBox)
+
+        let t = elem.snippet.title
+        let i = elem.id.videoId 
+        let video = {
+           t,
+           i,
+        }
+
+        movieBox.onclick = () => {
+            playVideo(video)
+        }
+
+    })
+}
+
+
+let playVideo = (video) => {
+    localStorage.setItem("video",JSON.stringify(video))
+}
+
+
+
+// // snippet:
+// // channelId: "UCah9jr4Ehlrt4Wv-q-CgJkg"
+// // channelTitle: "IndiaMarvel"
+// // description: "मैं सुनाता हूं कहानी एक स्पेस सिपाही की, थॉर ओडिनसन... देखिए मार्वल ..."
+// // liveBroadcastContent: "none"
+// // publishTime: "2022-05-24T03:16:09Z"
+// // publishedAt: "2022-05-24T03:16:09Z"
+// // thumbnails: {default: {…}, medium: {…}, high: {…}}
+// // title: "Marvel Studios&#39; Thor: Love and Thunder | Official Hindi Trailer | In Cinemas 8 July 2022"
